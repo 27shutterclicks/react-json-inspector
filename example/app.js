2a9a891c4e3fff3bc553288627d5c18b29c24d3b
Andrei I. Gere <114191180+27shutterclicks@users.noreply.github.com>
2023-11-29 21:48:11 +0000

    removed render

--- example/app.js
+++ example/app.js
@@ -7,13 +7,4 @@ var InteractiveSelection = require('./interactive-selection');
 var h = React.createElement;
 var data = require('./data.json');
 
-document.addEventListener('DOMContentLoaded', function() {
-    render(
-        h(Inspector, {
-            data: data,
-            onClick: console.log.bind(console),
-            interactiveLabel: InteractiveSelection
-        }),
-        document.getElementById('inspector')
-    );
-});
+
